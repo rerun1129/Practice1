7c0e4938a81eba3e7c6be8b71bfb176b032b0b4e
SunghyunOH <90834728+SunghyunOH@users.noreply.github.com>
2021-09-17 15:05:29 +0900

    성연님 커밋

---
+++ dd/hello.java
@@ -0,0 +1,9 @@
+
+public class hello {
+
+	public static void main(String[] args) {
+		System.out.println("안녕하세요!");
+
+	}
+
+}
---
+++ dd/helloworld.java
@@ -0,0 +1,9 @@
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("안녕하세요");
+
+	}
+
+}
